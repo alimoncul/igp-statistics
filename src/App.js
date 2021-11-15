40a551c22b459890269b0bd23d47e6037f8eb12b
alimoncul <alimoncul@gmail.com>
2021-11-15 18:08:44 +0300

    component: tyre

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { NavigationBar } from './components/NavigationBar';
+import { Tyre } from './components/Tyre';
 import './index.css';
 import Store from './Store';
 
@@ -7,6 +8,12 @@ function App() {
     return (
         <Store>
             <NavigationBar />
+            <Tyre type="SUPERSOFT" />
+            <Tyre type="SOFT" />
+            <Tyre type="MEDIUM" />
+            <Tyre type="HARD" />
+            <Tyre type="INTERMEDIATE" />
+            <Tyre type="WET" />
         </Store>
     );
 }
---
+++ src/components/Tyre.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const TYRE_TYPES = {
+    SUPERSOFT: 'bg-red-600',
+    SOFT: 'bg-yellow-400',
+    MEDIUM: 'bg-white',
+    HARD: 'bg-yellow-600',
+    INTERMEDIATE: 'bg-green-700',
+    WET: 'bg-blue-600'
+};
+
+const TYRE_CODES = {
+    SUPERSOFT: 'SS',
+    SOFT: 'S',
+    MEDIUM: 'M',
+    HARD: 'H',
+    INTERMEDIATE: 'I',
+    WET: 'W'
+};
+
+export const Tyre = (props) => {
+    const color = TYRE_TYPES[props.type];
+    const code = TYRE_CODES[props.type];
+    return (
+        <div className={`grid w-9 h-9 border-2 border-black rounded-full justify-items-center place-content-center ${color}`}>
+            <div className="grid w-5 h-5 rounded-full bg-black text-gray-300 justify-items-center place-content-center text-xs font-bold">
+                {code}
+            </div>
+        </div>
+    );
+};
