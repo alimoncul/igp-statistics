a47089b69f632a121434c644635c747d371018f2
alimoncul <alimoncul@gmail.com>
2022-01-29 18:21:30 +0300

    name function

--- src/lib/wearColor.js
+++ src/lib/wearColor.js
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/a/17267684/6382932
 
-export default function (percent) {
+export default function wearColor(percent) {
     const r = percent < 50 ? 255 : Math.floor(255 - ((percent * 2 - 100) * 255) / 100);
     const g = percent > 50 ? 255 : Math.floor((percent * 2 * 255) / 100);
     return 'rgba(' + r + ',' + g + ',0,0.4)';
